fcc9163bc74bb04787d3490f30a7ccbadfd8515a
Burak Yavuz <brkyvz@gmail.com>
2016-08-15 10:24:13 -0700

    fix task

--- src/main/scala/sbtsparkpackage/SparkPackagePlugin.scala
+++ src/main/scala/sbtsparkpackage/SparkPackagePlugin.scala
@@ -231,7 +231,7 @@ object SparkPackagePlugin extends AutoPlugin {
     normalizeName(names(1)) + "-" + version
   }
 
-  def spPackageKeys = Seq(spJar)
+  def spPackageKeys = Seq(spJar.value)
   lazy val spPackages: Seq[TaskKey[File]] =
     for(task <- spPackageKeys; conf <- Seq(Compile, Test)) yield (task in conf)
   lazy val spArtifactTasks: Seq[TaskKey[File]] = spMakePom +: spPackages
